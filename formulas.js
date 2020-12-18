63cf6a644f635c8607f79827741f300e7be8d87d
Alejandro Echeverria <alejandro@echev.com>
2020-12-18 18:22:48 -0300

    feat(formulas#price_in_euro): create formular to convert usd to eur

--- formulas.js
+++ formulas.js
@@ -11,3 +11,17 @@ function BOOK_PRICE(basePrice, bookCategory) {
   let price = basePrice - basePrice * discountPercent;
   return Math.max(minimumPrice, price);
 }
+
+function PRICE_IN_EURO(fixerAccessKey, price) {
+  const baseURL = "http://api.currencylayer.com/live?";
+  const queryParams = [
+    { key: "access_key", value: fixerAccessKey},
+    { key: "from", value: "USD"},
+    { key: "to", value: "EUR"},
+    { key: "amount", value: price.toString()},
+  ];
+  const queryString = queryParams.map(p => p.key + "=" + p.value).join("&");
+  const response = UrlFetchApp.fetch(baseURL + queryString);
+  const data = JSON.parse(response.getContentText());
+  return price * data.quotes.USDEUR;
+}
